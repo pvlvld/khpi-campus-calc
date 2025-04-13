a45d46e994f1cd08dc503b0bf04fda409f2760aa
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-04-13 20:26:39 +0300

    info comment for accessors

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@
 
 import {App} from "./app.js";
 
+// Calculator supports input from the keyboard as well as via mouse.
+// (there is brackets, btw, but i was too lazy to add them to the screen keyboard)
+
 const app = new App();
 app.init();
